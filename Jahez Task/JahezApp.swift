1c54521a2e46c0552df59ae2fcac0f0a699ea4b3
Ahmad Ghalib <ahmad.alsufy@orofi.xyz>
2026-04-26 23:49:26 +0300

    init

---
+++ Jahez Task/JahezApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jahez_TaskApp.swift
+//  Jahez Task
+//
+//  Created by Sufy on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Jahez_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
